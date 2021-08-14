8fc0570f00bd08c2510a5475d8bc1f78ac0328d2
Ahmed Ashraf <ahmed29329@gmail.com>
2021-08-14 09:46:54 +0200

    Update union_find.java

--- graph/union_find.java
+++ graph/union_find.java
@@ -1,39 +1,39 @@
 class UnionFind {
-        int root[];
-        int rank[];
+    int root[];
+    int rank[];
 
-        public UnionFind(int size) {
-            root = new int[size];
-            rank = new int[size];
-            for (int i = 0; i < size; i++) {
-                root[i] = i;
-                rank[i] = 1; 
-            }
-        }
-
-        public int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            return root[x] = find(root[x]);
-        }
-
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {
-                if (rank[rootX] > rank[rootY]) {
-                    root[rootY] = rootX;
-                } else if (rank[rootX] < rank[rootY]) {
-                    root[rootX] = rootY;
-                } else {
-                    root[rootY] = rootX;
-                    rank[rootX] += 1;
-                }
-            }
-        }
-
-        public boolean connected(int x, int y) {
-            return find(x) == find(y);
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1; 
         }
     }
+
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
